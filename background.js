c8fc3381985e469dac031ff3b65b8d04c3529d3d
parang <appdler96@kaist.ac.kr>
2020-03-16 16:00:28 +0900

    feat(background): Fix code pretty

--- background.js
+++ background.js
@@ -1,22 +1,31 @@
 const body = document.querySelector("body");
 
 function getImage(callbackFunc){
-    fetch("https://source.unsplash.com/1600x900/?nature")
-      .then(function(response) {
-          callbackFunc(response);
-      });
+  fetch("https://source.unsplash.com/1600x900/?nature")
+    .then(function(response) {
+      callbackFunc(response);
+    });
 }
 
-getImage(function(image){
+function init() {
+  getImage(function(image){
     console.log(image);
+    // add background container
+    const container = document.createElement("div");
+    body.appendChild(container);
+    container.classList.add("bg-container");
+  
     // add background image
     const img = document.createElement("img");
-    body.appendChild(img);
+    container.appendChild(img);
     img.src = image.url;
     img.classList.add("bg-image");
-    
+  
     // add cover on background image
     const div = document.createElement("div");
-    body.appendChild(div);
+    container.appendChild(div);
     div.classList.add("bg-cover");
-})
+  })
+}
+
+init();
